c57cbe2462bdcf5d8d20108e2d32be7228d2d198
mikayilov2006@gmail.com <mikayilov2006@gmail.com>
2024-11-13 01:00:52 +0400

    add hover

--- client/src/components/productHero.jsx
+++ client/src/components/productHero.jsx
@@ -3,10 +3,10 @@ import { products } from "../data/products";
 
 const Product = () => {
   return (
-    <section className="flex flex-wrap gap-5 px-5"> 
+    <section className="flex flex-wrap gap-5 px-5 "> 
       {products.map((items, key) => {
         return (
-          <div id={key} className=" w-[270px] shadow-xl shadow-gray-900 rounded-lg p-5 space-y-3 ">
+          <div id={key} className=" w-[270px] shadow-xl shadow-gray-900 rounded-lg p-5 space-y-3 hover:scale-105 transition-all duration-500  cursor-pointer">
             <img className=" rounded-2xl " src={items.img} alt="" />
             <h3>{items.title} <span>{items.feature.memory} <span>{items.feature.color}</span></span></h3>
             <h4>{items.price} azn</h4>
