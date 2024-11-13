5a91c26e97921ffd4b8f09ef062fa515ccfe5782
mikayilov2006@gmail.com <mikayilov2006@gmail.com>
2024-11-13 22:09:03 +0400

    Filter Praducts

--- client/src/components/productHero.jsx
+++ client/src/components/productHero.jsx
@@ -1,28 +1,72 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { products } from "../data/products";
 import { NavLink } from "react-router-dom";
 
 const Product = () => {
+  const [arrayModel, setModel] = useState([]);
+  const [arrayModelFilter, setModelFilter] = useState([]);
+
+  useEffect(() => {
+    const uniqueModels = new Set();
+    products.forEach((element) => {
+      const nameModel = element.title.split(" ")[0];
+      uniqueModels.add(nameModel);
+    });
+    setModel([...uniqueModels]);
+  }, []);
+
   function info(item) {
     localStorage.setItem("selectProduct", JSON.stringify(item));
   }
+  function handleCheckboxChange(e) {
+    const nameModel = e.target.innerText;
+    if (arrayModelFilter.includes(nameModel)) {
+      setModelFilter(arrayModelFilter.filter((model) => model !== nameModel));
+    } else {
+      setModelFilter([...arrayModelFilter, nameModel]);
+    }
+  }
+  const displayedProducts =
+    arrayModelFilter.length == 0
+      ? products
+      : products.filter((item) =>
+          arrayModelFilter.includes(item.title.split(" ")[0])
+        );
 
   return (
-    <section className="flex flex-wrap gap-5 px-5 ">
-      {products.map((item, key) => (
-        <NavLink 
-          onClick={() => info(item)} 
-          key={key} 
-          to="/main"
-          className="w-[270px] shadow-xl shadow-gray-900 rounded-lg p-5 space-y-3 hover:scale-105 transition-all duration-500 cursor-pointer"
-        >
-          <img className="rounded-2xl" src={item.img} alt={item.title} />
-          <h3>
-            {item.title} <span>{item.feature.memory} <span>{item.feature.color}</span></span>
-          </h3>
-          <h4>{item.price} azn</h4>
-        </NavLink>
-      ))}
+    <section className="flex gap-5 px-5 ">
+      <div className="w-[300px]">
+        <ul>
+          <h2>Marka</h2>
+          {arrayModel.map((item, key) => (
+            <li key={key} className="space-x-1">
+              <input type="checkbox" id={key}  />
+              <label htmlFor={key}>
+                <span onClick={handleCheckboxChange}>{item}</span>
+              </label>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="flex flex-wrap gap-5 px-5 ">
+        {displayedProducts.map((item, key) => (
+          <NavLink
+            onClick={() => info(item)}
+            key={key}
+            to="/main"
+            className="w-[270px] shadow-xl shadow-gray-900 rounded-lg p-5 space-y-3 hover:scale-105 transition-all duration-500 cursor-pointer"
+          >
+            <img className="rounded-2xl" src={item.img} alt={item.title} />
+            <h3>
+              {item.title}{" "}
+              <span>
+                {item.feature.memory} <span>{item.feature.color}</span>
+              </span>
+            </h3>
+            <h4>{item.price} azn</h4>
+          </NavLink>
+        ))}
+      </div>
     </section>
   );
 };
--- client/src/data/products.js
+++ client/src/data/products.js
@@ -7,6 +7,14 @@ export const products = [
         },
         img:"https://kontakt.az/media/catalog/product/cache/65f386b07961c74be97b1434cd633f1d/t/m/tm-dg-sbp-1105-sm-2911_1.png",
         price:2.899
+    },{
+        title:"Samsung Galaxy A25",
+        feature:{
+            memory:"256Gb",
+            color:"Light Blue"
+        },
+        img:"https://kontakt.az/media/catalog/product/cache/65f386b07961c74be97b1434cd633f1d/t/m/tm-dg-sbp-1105-sm-2697_1_1.png",
+        price:630
     },
     {
         title:"iPhone 16",
@@ -61,5 +69,41 @@ export const products = [
         },
         img:"https://kontakt.az/media/catalog/product/cache/65f386b07961c74be97b1434cd633f1d/t/m/tm-dg-sbp-1105-sm-2524_1.png",
         price:1.899
+    },
+    {
+        title:"Samsung Galaxy Z Flip 6",
+        feature:{
+            memory:"256Gb",
+            color:"Silver"
+        },
+        img:"https://kontakt.az/media/catalog/product/cache/65f386b07961c74be97b1434cd633f1d/t/m/tm-dg-sbp-1105-sm-2842_1.png",
+        price:2.399
+    },
+    {
+        title:"Samsung Galaxy A05S",
+        feature:{
+            memory:"128Gb",
+            color:"Light Violet"
+        },
+        img:"https://kontakt.az/media/catalog/product/cache/65f386b07961c74be97b1434cd633f1d/t/m/tm-dg-sbp-1105-sm-2593-1.png",
+        price:319
+    },
+    {
+        title:"HONOR 200 Pro",
+        feature:{
+            memory:"512Gb",
+            color:"Moonlight White"
+        },
+        img:"https://kontakt.az/media/catalog/product/cache/65f386b07961c74be97b1434cd633f1d/t/m/tm-dg-sbp-1105-sm-2856_1.png",
+        price:1.699
+    },
+    {
+        title:"HONOR X9B ",
+        feature:{
+            memory:"256Gb",
+            color:"Emerald Green"
+        },
+        img:"https://kontakt.az/media/catalog/product/cache/65f386b07961c74be97b1434cd633f1d/t/m/tm-dg-sbp-1105-sm-2607.png",
+        price:699
     }
 ]
